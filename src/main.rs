fff721619b13b7f9de399e547d9ebc0bcea97a99
Fu-Te <37261985+T3pp31@users.noreply.github.com>
2024-04-03 00:25:33 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use caesar_cipher_enc_dec::caesar_cipher::encrypt;
+use caesar_cipher_enc_dec::caesar_cipher::{decrypt, encrypt};
 fn main() {
     // 暗号化と復号化 --- (*1)
     let text: &str = "I LOVE YOU.";
     let enc_text: String = encrypt(&text, 3); // 暗号化
     let dec_text: String = encrypt(&enc_text, -3); // 復号化
+    let dec_text2: String = decrypt(&enc_text, 3);
     println!("文字列: {}", text);
     println!("暗号化: {}", enc_text);
     println!("復号化: {}", dec_text);
+    println!("復号化: {}", dec_text2);
 }
